aceaef36471f161a238e47affc656107659d0bc1
neil <neil.jheng@eyesmedia.com.tw>
2023-02-18 10:59:46 +0800

    spring boot line project first time commit

---
+++ src/main/java/com/demo/line/DemoLineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLineServiceApplication.class, args);
+	}
+
+}
